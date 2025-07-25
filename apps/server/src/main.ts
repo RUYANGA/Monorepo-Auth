4046901412ef1366b09ecaed97850deab2678707
RUYANGA <ruyangam15@gmail.com>
2025-07-25 15:58:22 +0200

    securuty

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
+  app.use(helmet())
   app.useGlobalPipes(
     new ValidationPipe({
       transform:true,
