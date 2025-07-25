55cd79e9337d89ab914f87ba6e69f7de60a8ad01
RUYANGA <ruyangam15@gmail.com>
2025-07-25 11:04:07 +0200

    create infrastucture

--- apps/server/src/shared/infrastructure/infrastructure.service.ts
+++ apps/server/src/shared/infrastructure/infrastructure.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class InfrastructureService {}
+export class InfrastructureService {
+    constructor(private readonly prisma:PrismaService){}
+
+    async checkRecordExists(model:string,id:string){
+        const record= await this.prisma[model].findUnique({
+            where:{id}
+        })
+        if(!record){
+            throw new NotFoundException(`${model} with id ${id} not found`)
+        }
+        return record
+    }
+
+    async checkDuplicate(model:string,fields:{property:string,value:any}){
+        
+    }
+}
