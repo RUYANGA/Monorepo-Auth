38f2595ccc5bef0cb25474b77534b4a7d894c3e1
RUYANGA <ruyangam15@gmail.com>
2025-07-25 16:30:54 +0200

    configure

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,16 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
-  app.use(helmet())
+  app.use(helmet());
+  app.use(
+    compression({
+      level: 6,
+      threshold: 1024,
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
-      transform:true,
-      whitelist:true
-    })
-  )
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  const port = process.env.PORT ?? 3001;
+  await app.listen(port);
+  console.log(`🚀 Server running on http://localhost:${port}`);
 }
 bootstrap();
