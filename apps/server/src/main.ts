0e2581ffe66804e186cd35783c16ff391c15b290
RUYANGA <ruyangam15@gmail.com>
2025-07-26 13:15:15 +0200

    .env

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors({
-    origin:[process.env.FRONTEND_URL,"http://localhost:3000"],
+    origin:"*",//[process.env.FRONTEND_URL,"http://localhost:3000"]
     Credential:true
   });
   app.use(
