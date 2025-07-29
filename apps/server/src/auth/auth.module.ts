5594d1b6744e8e88a3c902b817ca3b8588b8c8f8
RUYANGA <ruyangam15@gmail.com>
2025-07-29 16:36:22 +0200

    fix .env

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -19,6 +19,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
     }),
   ],
+  
   controllers: [AuthController],
   providers: [AuthService, InfrastructureService, PrismaService],
 })
